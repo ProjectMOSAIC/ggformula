e9c87e85d22a1f836988f3ff03b4394a954b2631
Randall Pruim <rpuim@gmail.com>
2020-07-12 22:22:56 -0400

    add ggridges interfaces

---
+++ R/ggridges.R
@@ -0,0 +1,110 @@
+#' Formula interface to ggridges plots
+#'
+#' Formula interface to ggridges plots
+#'
+#' @rdname ggridges
+#' @import ggridges
+#' @inheritParams ggridges::geom_ridgeline
+#' @export
+#' @examples
+#' data.frame(
+#'   x = rep(1:5, 3), y = c(rep(0, 5), rep(1, 5), rep(3, 5)),
+#'   height = c(0, 1, 3, 4, 0, 1, 2, 3, 5, 4, 0, 5, 4, 4, 1)
+#' ) %>%
+#'   gf_ridgeline(y ~ x, height = ~ height, group = ~y, fill = "lightblue", alpha = 0.7)
+
+gf_ridgeline <-
+  layer_factory(
+    geom = "ridgeline", stat = "identity", position = "identity",
+    aes_form = y ~ x,
+    extras = alist(height =, scale = 1, min_height = 0, color = , fill = , alpha = ,
+                   group =, linetype = , size = ,
+                   point_size =, point_shape = , point_colour = , point_fill = ,
+                   point_alpha = , point_stroke =)
+  )
+
+#' @rdname ggridges
+#' @seealso [`ggridges::geom_density_ridges()`]
+#' @section Details:
+#' Note that the default stat (`stat_density_ridges`) makes joint density estimation
+#' across all datasets. This may not generate the desired result when using
+#' faceted plots. As an alternative, you can set `stat = "density"` to use
+#' `stat_density()`. In this case, it is required to add the aesthetic mapping
+#' `height = stat(density)` (see examples).
+#'
+#' @export
+#' @examples
+#' diamonds %>%
+#'   gf_density_ridges(cut ~ price,
+#'     scale = 2, fill = ~ cut, alpha = 0.6, show.legend = FALSE) %>%
+#'   gf_theme(theme_ridges()) %>%
+#'   gf_refine(
+#'     scale_y_discrete(expand = c(0.01, 0)),
+#'     scale_x_continuous(expand = c(0.01, 0))
+#'   )
+#' diamonds %>%
+#'   gf_density_ridges(clarity ~ price | cut,
+#'     scale = 2, fill = ~ clarity, alpha = 0.6, show.legend = FALSE) %>%
+#'   gf_theme(theme_ridges()) %>%
+#'   gf_refine(
+#'     scale_y_discrete(expand = c(0.01, 0)),
+#'     scale_x_continuous(expand = c(0.01, 0))
+#'   )
+#' diamonds %>%
+#'   gf_density_ridges(clarity ~ price | cut, height = ~stat(density), stat = "density",
+#'     scale = 2, fill = ~ clarity, alpha = 0.6, show.legend = FALSE) %>%
+#'   gf_theme(theme_ridges()) %>%
+#'   gf_refine(
+#'     scale_y_discrete(expand = c(0.01, 0)),
+#'     scale_x_continuous(expand = c(0.01, 0))
+#'   )
+
+gf_density_ridges <-
+  layer_factory(
+    geom = "density_ridges", stat = "density_ridges", position = "points_sina",
+    aes_form = y ~ x,
+    extras = alist(height =, scale = 1, rel_min_height = 0,
+                   color = , fill = , alpha = , group =, linetype = , size = ,
+                   point_size =, point_shape = , point_colour = , point_fill = ,
+                   point_alpha = , point_stroke =,
+                   panel_scaling = TRUE)
+  )
+
+
+#' @rdname ggridges
+#' @seealso [`ggridges::geom_density_ridges()`]
+#' @export
+#' @examples
+#' diamonds %>%
+#'   gf_density_ridges2(cut ~ price, scale = 2, fill = ~ cut, alpha = 0.6, show.legend = FALSE) %>%
+#'   gf_theme(theme_ridges()) %>%
+#'   gf_refine(
+#'     scale_y_discrete(expand = c(0.01, 0)),
+#'     scale_x_continuous(expand = c(0.01, 0))
+#'   )
+
+gf_density_ridges2 <-
+  layer_factory(
+    geom = "density_ridges2", stat = "density_ridges", position = "points_sina",
+    aes_form = y ~ x,
+    extras = alist(height =, scale = 1, rel_min_height = 0,
+                   color = , fill = , alpha = , group =, linetype = , size = ,
+                   point_size =, point_shape = , point_colour = , point_fill = ,
+                   point_alpha = , point_stroke =,
+                   panel_scaling = TRUE)
+  )
+
+#' @rdname ggridges
+#' @seealso [`ggridges::geom_density_ridges()`]
+#' @export
+#' @examples
+#' gf_density_line
+#'
+#'
+gf_density_line <-
+  layer_factory(
+    geom = "density_line", stat = "density", position = "identity",
+    aes_form = list(~ x, y ~ .),
+    extras = alist(color = , fill = , alpha = 0.5, group =, linetype = , size = ,
+                   kernel = "gaussian", n = 512, trim = FALSE)
+  )
