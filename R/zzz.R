8f2319a08e796fc1acf2f371ffbb92691996a86a
Randall Pruim <rpruim@gmail.com>
2026-01-16 21:36:49 -0500

    Don't attach ggiraph in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,9 @@
 .onAttach <- function(libname, pkgname) {
+  # # attach ggiraph for interactive functions
+  # if (requireNamespace("ggiraph", quietly = TRUE)) {
+  #   library(ggiraph, warn.conflicts = FALSE)
+  # }
 
-    # attach ggiraph for interactive functions
-    if (requireNamespace("ggiraph", quietly = TRUE)) {
-      library(ggiraph, warn.conflicts = FALSE)
-    }
-  
   packageStartupMessage(
     paste(
       "\nNew to ggformula?  Try the tutorials: ",
