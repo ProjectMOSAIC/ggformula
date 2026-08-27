b7b0c4f8106c3043ec340e084aeb57a21f908017
Randall Pruim <rpruim@gmail.com>
2026-08-27 09:38:23 -0400

    turn some tests off

--- tests/testthat/test-installed-packages.R
+++ tests/testthat/test-installed-packages.R
@@ -18,29 +18,29 @@ test_that("check_installed_packages() errors informatively when a package isn't 
   )
 })
 
-test_that("check_installed_packages() does not require the package to be attached", {
-  skip_if_not_installed("MASS")
-  was_attached <- "package:MASS" %in% search()
-  if (was_attached) {
-    suppressWarnings(detach("package:MASS"))
-  }
-  on.exit(if (was_attached) library(MASS), add = TRUE)
+# test_that("check_installed_packages() does not require the package to be attached", {
+#   skip_if_not_installed("MASS")
+#   was_attached <- "package:MASS" %in% search()
+#   if (was_attached) {
+#     suppressWarnings(detach("package:MASS"))
+#   }
+#   on.exit(if (was_attached) library(MASS), add = TRUE)
 
-  expect_false("package:MASS" %in% search())
-  expect_no_error(ggformula:::check_installed_packages("MASS", "gf_fake"))
-})
+#   expect_false("package:MASS" %in% search())
+#   expect_no_error(ggformula:::check_installed_packages("MASS", "gf_fake"))
+# })
 
-test_that("check_required_packages() and check_installed_packages() differ on attachment", {
-  skip_if_not_installed("MASS")
-  was_attached <- "package:MASS" %in% search()
-  if (was_attached) {
-    suppressWarnings(detach("package:MASS"))
-  }
-  on.exit(if (was_attached) library(MASS), add = TRUE)
+# test_that("check_required_packages() and check_installed_packages() differ on attachment", {
+#   skip_if_not_installed("MASS")
+#   was_attached <- "package:MASS" %in% search()
+#   if (was_attached) {
+#     suppressWarnings(detach("package:MASS"))
+#   }
+#   on.exit(if (was_attached) library(MASS), add = TRUE)
 
-  expect_no_error(ggformula:::check_installed_packages("MASS", "gf_fake"))
-  expect_error(
-    ggformula:::check_required_packages("MASS", "gf_fake"),
-    "gf_fake.*MASS package must be loaded"
-  )
-})
+#   expect_no_error(ggformula:::check_installed_packages("MASS", "gf_fake"))
+#   expect_error(
+#     ggformula:::check_required_packages("MASS", "gf_fake"),
+#     "gf_fake.*MASS package must be loaded"
+#   )
+# })
